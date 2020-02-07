dd986c4511216a0eb0696ee8737a5a1f95e20fea
Mike Rix Wolfe <michaelrixwolfe@gmail.com>
2020-02-07 15:43:32 -0600

    updates

--- app/views/index.py
+++ app/views/index.py
@@ -5,7 +5,6 @@ from app.persist.models import Message
 
 @app.route('/gallery', strict_slashes=False, methods=['GET'])
 def index():
-    messages = [dict({"MediaUrl": m.MediaUrl, "DateReceived": str(m.DateReceived), "Body": m.Body, "From": m.From}) for m in
-            Message.query.filter(Message.AccountSid==app.config['TWILIO_ACCOUNT_SIDS_CSV']).filter(Message.To==app.config['PHONE_NUMBER']).filter(Message.MediaUrl!=None).all()]
+    messages = Message.query.filter(Message.AccountSid==app.config['TWILIO_ACCOUNT_SIDS_CSV']).filter(Message.To==app.config['PHONE_NUMBER']).filter(Message.MediaUrl!=None).all()
     return render_template('index.html', msgs=messages)
 
